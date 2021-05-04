19d98ab5ce51953b975a1230255167e0d7dee9b5
piremonte <42988421+piremonte@users.noreply.github.com>
2021-05-04 00:31:44 -0400

    Re-direccionamiento
    Se mando a diferentes pantallas y rutas, a los usuarios dependiendo su rol

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -6,6 +6,7 @@ export const authReducer = (state = {}, action) => {
       return {
         uid: action.payload.uid,
         name: action.payload.displayName,
+        rol: action.payload.rol,
       };
 
     case types.logout:
