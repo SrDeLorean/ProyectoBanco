a78887edfbb0dcacddbf77e70b81088b17645fa8
Jesus Moris <jesus@soluciontotal.cl>
2021-06-12 19:38:46 -0400

    Se cambia login a rut y clave

--- Back/routes/api.php
+++ Back/routes/api.php
@@ -27,7 +27,7 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-    Route::post('login', [AuthController::class, "login"]);
+    Route::post('login', [AuthController::class, "login2"]);
     Route::post('logout', [AuthController::class, "logout"]);
     Route::post('refresh', [AuthController::class, "refresh"]);
     Route::get('me', [AuthController::class, "me"]);
